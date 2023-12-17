6eb338db2bfc9bd5740fac0980089bcc9ca5ece0
Carmelleonie <sagboleonie7@gmail.com>
2023-12-17 20:08:30 +0100

    monty

--- opcode_4.c
+++ opcode_4.c
@@ -8,7 +8,18 @@
 */
 void _stack(stack_t **stack, unsigned int line_number)
 {
+	stack_t *top = *stack;
+	(void) line_number;
 
+	if (!stack || !*stack || !(*stack)->next)
+		return;
+	(*stack)->next->prev = NULL;
+	while (top->next)
+		top = top->next;
+	top->next = *stack;
+	(*stack) = (*stack)->next;
+	top->next->next = NULL;
+	top->next->prev = top;
 }
 /**
 *_queue -  sets the format of the data to a queue (FIFO)
@@ -18,5 +29,21 @@ void _stack(stack_t **stack, unsigned int line_number)
 */
 void _queue(stack_t **stack, unsigned int line_number)
 {
+	stack_t *top;
+	(void) line_number;
+
+	if (!stack || !*stack || !(*stack)->next)
+		return;
+
+	top = *stack;
+	while (top->next)
+		top = top->next;
+
+	top->next = *stack;
+	top->prev->next = NULL;
+	top->prev = NULL;
+
+	(*stack)->prev = top;
+	*stack = top;
 
 }
